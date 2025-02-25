a40e9a1d7f004e79fbbfc816a749c20c45ce4e25
amanmdest <amanmdest@gmail.com>
2025-02-25 20:33:52 -0300

    ajustes url deploy.

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const { NODE_ENV } = process.env
 // const URL = NODE_ENV === "development" ? "localhost:3000/api" : "/api";
-const URL = "http://localhost:3000/api";
+const URL = "https://fullstackspotifyreplica.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
